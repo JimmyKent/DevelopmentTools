9fd26c29399174a874db099b482cd4de3ecf73d3
jinguochong <jinguochong@gmail.com>
2017-09-16 10:39:12 +0800

    io read line

--- IOLib/src/test/java/com/jimmy/development/iolib/IOTest.java
+++ IOLib/src/test/java/com/jimmy/development/iolib/IOTest.java
@@ -1,52 +1,139 @@
 package com.jimmy.development.iolib;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
+import java.io.LineNumberReader;
 import java.nio.channels.FileChannel;
+import java.util.Scanner;
 
 /**
  * http://nadeausoftware.com/articles/2008/02/java_tip_how_read_files_quickly
+ * 看api
+ * XXX 考虑线程安全
  */
 public class IOTest {
 
     private static String name = "";
+    private static final String READ_FILE_TEST = "/Users/jinguochong/AndroidStudioProjects/DevelopmentTools/IOLib/src/test/java/com/jimmy/development/iolib/ReadTest.txt";
     private static final int SIZE = 8 * 1024;//8k byte is the best.
 
-    public static void main(String[] args) {
+    @Test
+    public void testReadLine() {
+        int[] lineNums = {0, 1, 10};//file line's size is 5.
+        for (int i : lineNums) {
+
+            String line = readSpecificLine(READ_FILE_TEST, i);//从0开始
+
+            String line2 = readSpecificLine2(READ_FILE_TEST, i);
+
+            System.out.println("line:" + line);
+            System.out.println("line2:" + line2);
+
+            if (i == 10) {
+                Assert.assertNull(line);
+                Assert.assertNull(line2);
+            } else {
+                Assert.assertEquals(line, i + "");
+                Assert.assertEquals(line2, i + "");
+            }
+
+
+        }
 
 
     }
-    //InputStream/OutputStream 面向字节
+
+    @Test
+    public void testReadMultiLines() {
+        //读多行
+
+    }
+
+    @Test
+    public void testFileSize() {
+
+    }
+
+    @Test
+    public void testFileMD5() {
+
+    }
+
+    @Test
+    public void testNio() {
+
+    }
+
+    @Test
+    public void testScanner() {
+        //Scanner scanner = new Scanner();
+    }
+
+    @Test
+    public void testWriteFile() {
+
+    }
+
+    public void testEncode() {
+
+    }
+
+    public void testGzip() {
+        
+    }
+
+    public void testSerialize() {
+
+    }
+
+
+    //InputStream/OutputStream 面向字节,所以对于readLine这种操作是没有的,read和skip都是针对字节的.
     //InputStreamReader/OutputStreamWrite 转换InputStream-->Reader
+
     //Reader/Write 面向字符
 
     //RandomAccessFile 适用于大小已知的文件. seek
     //RandomAccessFile 1.4之后大部分功能由nio存储隐射文件取代
 
-    //InputStream 的available: 在没有阻塞的情况下所能读取的字节数,要谨慎使用
 
     //LineNumberReader 行数相关
 
-    
+    // Scanner 类
+
+    //通过nio读写文件
+    //通道,缓冲器 ByteBuffer
+    //引入nio之后,FileInputStream/FileOutputStream/RandomAccessFile 被修改,用来产生 FileChannel
+    //FileChannel
+
+    //编解码
+    //获取基本类型
+    //视图缓冲器
+    //...
+    //内存映射文件 -- 解决超大文件
+    //文件加锁
+    //部分加锁
+    //压缩//Gzip
+    //序列化 transient
 
 
-    //读文件 thinking in java 有几种Reader
+    //读文件
     //按行读取
     //读取特定行
     //跳过
-
+    //读文件大小
+    //读文件MD5
 
     //写文件
     //写最后一行
     //写第一行
     //写特定行
-
-
-    //读文件大小
-    //读文件MD5
 
 
     /**
@@ -75,6 +162,7 @@ public class IOTest {
      * FileInputStream with byte array reads
      * FileInputStream does an I/O operation on every read and it synchronizes on all method calls to make it thread-safe.
      * To reduce this overhead, read multiple bytes at once into a buffer array of bytes
+     *
      * @return
      */
     private static long read2() {
@@ -93,6 +181,63 @@ public class IOTest {
         return checkSum;
     }
 
+
+    //---- readLine需要针对字符的 Reader Scanner 或者 RandomAccessFile
+    //BufferedReader LineNumberReader
+    private static String readSpecificLine(String fileName, int line) {
+        String data = null;
+        BufferedReader reader = null;
+
+        try {
+            reader = new BufferedReader(new FileReader(fileName));
+            int i = 0;
+            while ((data = reader.readLine()) != null && i < line) {
+                i++;
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return data;
+    }
+
+    private static String readSpecificLine2(String fileName, int line) {
+        String data = null;
+        LineNumberReader reader = null;
+
+        try {
+            reader = new LineNumberReader(new FileReader(fileName));
+            reader.setLineNumber(line);
+            int i = 0;
+            while ((data = reader.readLine()) != null && i < line) {
+                i++;
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return data;
+    }
+
+
+    //FileReader f=new FileReader("test.txt");
+    //LineNumberReader l=new LineNumberReader(f);
 
     /*Conclusions
     For the best Java read performance, there are four things to remember:
@@ -133,5 +278,21 @@ public class IOTest {
                 }
             }
         }
+    }
+
+    //InputStream 的 available 非线程安全
+    //InputStream 的available: 在没有阻塞的情况下所能读取的字节数,要谨慎使用
+    public static long getFileSize2(String path) {//错误示范
+        int len = -1;
+        try {
+            FileInputStream fis = new FileInputStream(path);
+            len = fis.available();
+            fis.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return len;
     }
 }
