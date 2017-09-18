0ef048ee3bdd7962467779f90cbb03b675fd8951
jinguochong <jinguochong@gmail.com>
2017-09-18 21:52:43 +0800

    annotation todo list

---
+++ Annotation/src/main/java/com/jimmy/development/annotation/AnnotationMethod.java
@@ -0,0 +1,12 @@
+package com.jimmy.development.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+/**
+ * Created by jinguochong on 2017/9/16.
+ */
+
+public @interface  AnnotationMethod {
+
+}
---
+++ Annotation/src/test/java/com/jimmy/development/annotation/AnnotationLearning.java
@@ -0,0 +1,43 @@
+package com.jimmy.development.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+
+/**
+ * Created by jinguochong on 2017/9/16.
+ */
+
+public class AnnotationLearning {
+
+//    @Target           注解用在什么地方
+//    ANNOTATION_TYPE,
+//    CONSTRUCTOR,      构造器
+//    FIELD,            域声明(包括enum实例)
+//    LOCAL_VARIABLE,   局部声明
+//    METHOD,           方法声明
+//    PACKAGE,          包声明
+//    PARAMETER,        参数声明
+//    TYPE,             类,接口(包括注解类型)或者enum声明
+//    TYPE_PARAMETER,
+//    TYPE_USE;
+
+//    @Retention()      需要在什么级别保存该注解信息
+
+//    @Documented       将此注解包含在Javadoc里面
+
+//    @Inherited        允许子类继承父类的注解
+
+
+    //编写注解处理器
+
+
+
+
+    
+
+
+
+
+
+}
